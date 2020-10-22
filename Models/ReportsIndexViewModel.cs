b5011f8e33b3190f1d2665e89d10eebed707a3fb
Alejandro Cortez <alejandro17198@gmail.com>
2020-10-22 06:06:12 -0300

    Overhaul de reportes
    En vez de tener un botón para informar contagio, añadí toda una página de Reportes donde se puede informar contagios, dadas de alta o tests negativos según se requiera.

---
+++ Models/ReportsIndexViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TP1_ARQWEB.Models
+{
+    public class ReportsIndexViewModel
+    {
+        public string UserStateString;
+        public bool UserInfected;
+        public bool UserAtRisk;
+
+    }
+}
