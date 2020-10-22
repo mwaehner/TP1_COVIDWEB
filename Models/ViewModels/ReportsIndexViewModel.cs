99d6c14e9bc630d558a30090b1d31b22864b29a3
Alejandro Cortez <alejandro17198@gmail.com>
2020-10-22 06:30:18 -0300

    Nueva carpeta para ViewModels

---
+++ Models/ViewModels/ReportsIndexViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TP1_ARQWEB.Models
+{
+    public class ReportsIndexViewModel
+    {
+        public string UserStateString { get; set; }
+        public bool UserInfected { get; set; }
+        public bool UserAtRisk { get; set; }
+
+    }
+}
